87354f8432f4a58b519ebce253a4ddd4485e15a2
Cyril Baudrillart <cyril.baudrillart@gmail.com>
2025-11-17 17:06:10 +0100

    fix bug on tests

--- tests/test_operations.py
+++ tests/test_operations.py
@@ -1,11 +1,17 @@
+import pytest
 from src.math_operations import add, sub
 
-def test_add(a,b):
-    assert add(2,3)==5
-    assert(-1,1)==0
+@pytest.mark.parametrize("a, b, expected", [
+    (1, 2, 3),
+    (5, 3, 8),
+    (0, 0, 0),
+])
+def test_add(a, b, expected):
+    assert add(a, b) == expected
 
-def test_sub(a,b):
-    assert sub(5,3)==2
-    assert sub(4,3)==1
-    assert sub(3,3)==0
-    assert sub(2,3)==-1
+@pytest.mark.parametrize("a, b, expected", [
+    (3, 2, 1),
+    (5, 8, -3),
+])
+def test_sub(a, b, expected):
+    assert sub(a, b) == expected
